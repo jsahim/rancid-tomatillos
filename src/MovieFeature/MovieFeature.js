a2abd81afbf06c5dc7c6b197076aca51b806bf9c
Jeff Sahim <jeffreysahim@gmail.com>
2023-03-24 17:07:55 -0400

    Update MovieFeature component and render()

--- src/MovieFeature/MovieFeature.js
+++ src/MovieFeature/MovieFeature.js
@@ -5,37 +5,32 @@ class MovieFeature extends Component {
   constructor ({movie}) {
     super();
     this.state = {
-      clickedMovie: movie
+      clickedMovie: {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }
     }
   }
 
-  onClose = e => {
+  goHome = e => {
     this.props.onClose && this.props.onClose(e);
   };
 
   render() {
-    if (!this.props.show) {
-      return null;
-    }
+    const movie = this.state.clickedMovie
     return (
-      <div className="modal micromodal-slide" id="modal">
-        <div className="content">{this.props.children}</div>
-        <div className="actions">
-          <button className="modal-btn" onClick={this.onClose}>Close</button>
-          {/* <div className="modal-overlay" tabindex="-1" data-micromodal-close>
-            <div className="modal-container" role="dialog" aria-modal="true" aria-labelledby="modal-title">
-              <header className="modal-header">
-                <h2 className="modal-title" id="modalTitle">CONFIRMATION</h2>
-              </header>
-              <main className="modal-content" id="modalContent">
-              </main>
-              <footer className="modal-footer">
-                <button className="modal-btn button" data-micromodal-close aria-label="Close this dialog window" id="modalClose">Close</button>
-              </footer>
-            </div>
-          </div> */}
+      <section className="single-movie-details">
+        <div className="content">
+          <h2>{movie.title}</h2>
+          <img src={movie.backdrop_path} alt="Movie Backdrop"></img>
+          <p className="overview">{movie.overview}</p>
+          <p className="release-date">{movie.release_date}</p>
+          <p className="rating">{movie.average_rating}</p>
+          <p className="genre">{movie.genres}</p>
+          <p className="budget">{movie.budget}</p>
+          <p className="revenue">{movie.revenue}</p>
+          <p className="runtime">{movie.runtime}</p>
+          <p className="tagline">{movie.tagline}</p>
+          <button className="home-button" onClick={this.goHome}>Home</button>
         </div>
-      </div>
+      </section>
     );
   }
 }
