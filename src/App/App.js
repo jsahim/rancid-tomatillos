a57a4065a54d402dda3be9c2d3c1c35054cdc001
Jeff Sahim <jeffreysahim@gmail.com>
2023-03-24 17:35:54 -0400

    Add setClickMovie method

--- src/App/App.js
+++ src/App/App.js
@@ -8,20 +8,29 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      selectedMovie: []
+    }
   }
 
   // displayAllGenres = () => {
   //   return this.genres.map(genre => <GenreContainer genreName={genre} />)
   // }
 
+  setClickedMovie = (e) => {
+    const mov = e.target.id
+    const selectedMovie = [...this.state.selectedMovie, mov]
+    this.setState({selectedMovie})
+  }
+
   render() {
-    console.log(movieData)
+    const clickedMovie = {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }
     return (
       <main>
         <Navigation />
         <GenreContainer data={movieData} />
         {/* {this.displayAllGenres()} */}
-        <MovieFeature />
+        <MovieFeature clickedMovie={clickedMovie}/>
       </main>
     )
   }
