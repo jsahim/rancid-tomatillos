24909a982c3d90c1076e84be8b1a69f23d5cc33f
Jeff Sahim <jeffreysahim@gmail.com>
2023-03-28 19:04:19 -0400

    Add Nav styling and JS adjustment

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Nav.css"
 
 const Navigation = () => {
   return (
     <header className="header">
-      <h1>Rancid Tomatillos</h1>
+      <h1>🍅 Rancid Tomatillos</h1>
       <img src="" alt="site"></img>
       <img src="" alt="user Icon"></img>
     </header>
