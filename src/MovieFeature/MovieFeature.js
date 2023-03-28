375cbb4928fb8959eb028196f0f7882057433edb
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-03-28 16:59:23 -0700

    Update Movie Feature display

--- src/MovieFeature/MovieFeature.js
+++ src/MovieFeature/MovieFeature.js
@@ -3,26 +3,29 @@ import PropTypes from 'prop-types';
 import "./MovieFeature.css";
 
 const MovieFeature = ({clickedMovie, homeClicked, videos}) => {
+  console.log(videos)
   let trailer = videos.find(video => video.site === 'YouTube' && video.type === 'Trailer')
+  console.log(trailer)
   let movieKey
+
   !trailer == [] ? movieKey = trailer.key : movieKey = false 
   return (
     <section className="single-movie-details">
       <div className="content">
-        <h2>{clickedMovie.title}</h2>
         <img src={clickedMovie.backdrop_path} alt="Movie Backdrop"></img>
         <div>
+          <h2>{clickedMovie.title}</h2>
           <p className="overview">{clickedMovie.overview}</p>
-          <p className="release-date">{clickedMovie.release_date}</p>
-          <p className="rating">{clickedMovie.average_rating}</p>
-          <p className="genre">{clickedMovie.genres}</p>
-          <p className="budget">{clickedMovie.budget}</p>
-          <p className="revenue">{clickedMovie.revenue}</p>
-          <p className="runtime">{clickedMovie.runtime}</p>
+          <p className="release-date">Release Date: {clickedMovie.release_date}</p>
+          <p className="rating">Rating: {clickedMovie.average_rating}/10🫑</p>
+          <p className="genre">Genres: {clickedMovie.genres}</p>
+          <p className="budget">Budget: ${clickedMovie.budget}</p>
+          <p className="revenue">Total Revenue: ${clickedMovie.revenue}</p>
+          <p className="runtime">Runtime: {clickedMovie.runtime} mins</p>
           <p className="tagline">{clickedMovie.tagline}</p>
-          {movieKey ? <iframe src={`https://www.youtube.com/embed/${trailer.key}`} title={`${clickedMovie.title} Trailer`}></iframe> : <p>Bish it an't it</p>}
+          {movieKey ? <iframe src={`https://www.youtube.com/embed/${trailer.key}`} title={`${clickedMovie.title} Trailer`}></iframe> : <p>We're sorry there is no available Trailer...</p>}
+          <button className="home-button" onClick={() => homeClicked()}>Home</button>
         </div>
-        <button className="home-button" onClick={() => homeClicked()}>Home</button>
       </div>
     </section>
   );
