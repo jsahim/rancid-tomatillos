e2834a8683119f1d8a26afb5d5e56e1957246d68
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-03-28 17:00:19 -0700

    Update Nav bar styling

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,7 +4,7 @@ import "./Nav.css"
 const Navigation = () => {
   return (
     <header className="header">
-      <h1>🍅 Rancid Tomatillos</h1>
+      <h1>🫑Rancid Tomatillos</h1>
       <img src="" alt="site"></img>
       <img src="" alt="user Icon"></img>
     </header>
